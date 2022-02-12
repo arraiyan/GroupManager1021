3f1715bc5f1595383a483856f23f36b8621defc3
arraiyan <77921265+arraiyan@users.noreply.github.com>
2022-02-13 02:01:08 +0600

    c

--- inf.py
+++ inf.py
@@ -9,17 +9,17 @@ class env :
 
     🔷Our Big BitBull (BTB coin)  is going to live soon in many more exchanges 
 
-    🔷Currently we are on BANKCEX exchange Singapore
+    🔷Currently we are on BANKCEX exchange Singapore 
     
     🔷Anyone can trade there in our coin BTBC
-    
-    🔷Bulls oceans Ltd has taken over Aura Techno International Inc from DELAWARE with all its assets & liability through international acquisitions standards and hence forth all members of the AURA BITCOIN community will be incorporated and in this way BIG BITBULL gets a ready community.
+
+    🔷Based out of London & with over 15 years experience in asset management, they’ve continued to work closely with many software developers in Cryptocurrency.  Today they would like to present to you their BTB TOKEN Big Bitbull. The BTB token is built on the Binance Smart Chain Network and has a total supply of only 29.9 million. This is indeed a great opportunity for you to make your footprints in the digital world of Cryptocurrency. Join Bulls Oceans ltd. today in building a great community for our BTB ecosystem. Their latest project NFT coming soon on MUSIC coulee be a game changer, which will cater to more than 800 Million musicians registered worldwide. They promise you that BTB could provide you the best income opportunity through their scientific and organic trading platform’s.
 
     🔷My dear associates, we’re offering you a very innovative and lucrative business plan; for more details please log into bq3networks.io
 
     🔷Thank you all for your support and faith in Bulls Oceans Ltd & the Big Bitbull token. Together, you can make history in this world of cryptocurrency. 
 
-Thank you.💕    
+Thank you.💕  
     """
     HelpText = """
     Please contact with our admins
